c2a5c18e62c31d381715a83eebc52207f355f854
zx9951956 <57282802+zx9951956@users.noreply.github.com>
2019-12-31 16:31:20 +0800

    購物車70%

--- src/Components/pages/購物車.js
+++ src/Components/pages/購物車.js
@@ -19,7 +19,9 @@ class Cart extends React.Component{
                     <div style={{width:"100%",display:"flex",justifyContent:"center"}}>
                         <div className="Mycontainer" id="MainContainer" style={{paddingTop:"20px"}}>
                             <div className="TitleDiv" style={CenterStyle}>
-                                <h2 style={{color:"white",paddingTop:"120px"}}>您的購買紀錄</h2>
+                                <h2 style={{color:"#FF6100",paddingTop:"120px"}}>Step.1 購物車清單</h2>
+                                <h4 style={{color:"white",paddingTop:"125px"}}>→→→→</h4>
+                                <h5 style={{color:"white",paddingTop:"125px"}}>Step.2 購買成功</h5>
                             </div>
     
                             <GameTable contact={{tableType:"Cart"}}/>
